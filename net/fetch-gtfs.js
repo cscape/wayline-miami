596d12ea59b9d653a0247efb755e32ee4de2e3b1
ciolt <cioltdev@outlook.com>
2019-03-27 14:16:30 -0400

    switch to arraybuffer response, now works for saving gtfs

--- net/fetch-gtfs.js
+++ net/fetch-gtfs.js
@@ -1,7 +1,9 @@
 const axios = require('axios')
 
 const GTFSFetcher = async (url = 'https://www.miamidade.gov/transit/googletransit/current/google_transit.zip') => {
-  const { data } = await axios.get(url)
+  const { data } = await axios.get(url, {
+    responseType: 'arraybuffer'
+  })
   return data // binary zip
 }
 
