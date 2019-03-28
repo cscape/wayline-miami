3b75db22b11dca111828bbdcf8b221a883f49b07
ciolt <cioltdev@outlook.com>
2019-03-27 21:01:41 -0400

    add gtfs utilities

---
+++ functions/gtfs-util.js
@@ -0,0 +1,24 @@
+const realtime = require('gtfs-realtime-bindings')
+
+const GTFSUtil = {
+  clone: obj => JSON.parse(JSON.stringify(obj)),
+  makeMessageTemplate: (timestamp = Date.now()) =>
+    new realtime.FeedMessage({
+      header: new realtime.FeedHeader({
+        gtfs_realtime_version: '1.0',
+        timestamp
+      })
+    }),
+  genEnglishString: (text) => {
+    const translation = new realtime.TranslatedString()
+    translation.translation.push(new realtime.TranslatedString.Translation({
+      language: 'en',
+      text: text
+    }))
+    return translation
+  },
+  getEpochTime: (timeStr) => timeStr ? (parseInt(timeStr, 10) / 1000) : null,
+  isArray: Array.isArray
+}
+
+module.exports = GTFSUtil
