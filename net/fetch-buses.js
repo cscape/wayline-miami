5c84bd2ea29f211e0c30650e5c296611ffd5bcc0
ciolt <cioltdev@outlook.com>
2019-04-14 16:32:59 -0400

    Switch bus fetcher to use verbose endpoint

--- net/fetch-buses.js
+++ net/fetch-buses.js
@@ -1,12 +1,12 @@
 const axios = require('axios')
-const { Buses } = require('@wayline/transformer').MiamiDadeTransit
+const { BusesVerbose } = require('@wayline/transformer').MiamiDadeTransit
 const { xml2json } = require('@wayline/transformer').utils
-const basefeed = require('@wayline/config').basefeeds.MiamiDadeTransit
+const basefeed = require('@wayline/config').basefeeds.MiamiDadeTransitVerbose // Note it's verbose
 const endpoint = basefeed + 'Buses'
 
 const Fetcher = async (url = endpoint) => {
   const { data } = await axios.get(url)
-  const cleaned = Buses(xml2json(data))
+  const cleaned = BusesVerbose(xml2json(data))
   return cleaned
 }
 
