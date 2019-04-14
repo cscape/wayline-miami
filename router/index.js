e253199a4e563820efbe8087b0d921c5ff6bb20b
ciolt <cioltdev@outlook.com>
2019-04-14 17:18:05 -0400

    update index to show server name/ version

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const fs = require('fs')
+const pkg = require('../package.json')
 
 const filesrc = dir => path.join(process.cwd(), dir)
 const { TRANSIT_AGENCY = 'Miami-Dade Transit' } = process.env
@@ -12,7 +13,10 @@ router.get('/gtfs.zip', (req, res) => res.sendFile(filesrc('/tmp/gtfs.zip')))
 
 router.all('/', (req, res) => {
   const template = fs.readFileSync(filesrc('./static/.index.html'), 'utf8')
-  const intpl = template.replace(/(\$TRANSIT_AGENCY)/gm, TRANSIT_AGENCY)
+  const intpl = template
+    .replace(/(\$TRANSIT_AGENCY)/gm, TRANSIT_AGENCY)
+    .replace(/(\$PKGNAME)/gm, pkg.name)
+    .replace(/(\$PKGVERSION)/gm, pkg.version)
   res.setHeader('Content-Type', 'text/html')
   res.status(200).send(intpl)
 })
