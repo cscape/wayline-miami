1966700a186899bbb5d0006300de988d97f88303
ciolt <cioltdev@outlook.com>
2019-05-13 22:20:52 -0400

    switch to TrainsVerbose

--- net/fetch-metrorail.js
+++ net/fetch-metrorail.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-const { Trains } = require('@wayline/transformer').MiamiDadeTransit
+const { TrainsVerbose } = require('@wayline/transformer').MiamiDadeTransit
 const { xml2json } = require('@wayline/transformer').utils
 const basefeed = require('@wayline/config').basefeeds.MiamiDadeTransit
 const endpoint = basefeed + 'Trains'
@@ -7,7 +7,7 @@ const endpoint = basefeed + 'Trains'
 const Fetcher = async (url = endpoint) => {
   try {
     const { data } = await axios.get(url)
-    const cleaned = Trains(xml2json(data))
+    const cleaned = TrainsVerbose(xml2json(data))
     return cleaned
   } catch (err) {
     throw new Error(`Failed while fetching ${url}: ${err}`)
