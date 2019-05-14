e3bf0d09f0a1ce3ee90e22dc14596ecc60148a4a
ciolt <cioltdev@outlook.com>
2019-05-14 12:55:57 -0400

    Change to local VP in decoder test

--- tests/decode-vp.js
+++ tests/decode-vp.js
@@ -5,7 +5,7 @@ const path = require('path')
 // Deconstruct these classes from GTFS Realtime Bindings
 const { FeedMessage } = gtfsRB
 
-const rawFeed = readFileSync(path.join(__dirname, '../tmp/realtime/MBTA.pb')) // buffer
+const rawFeed = readFileSync(path.join(__dirname, '../tmp/realtime/VehiclePositions.pb')) // buffer
 
 const decodedMessage = FeedMessage.decode(rawFeed)
 
