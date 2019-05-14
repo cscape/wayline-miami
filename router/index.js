8c0237dd8a3f3897109f373eb9b16ba657532548
ciolt <cioltdev@outlook.com>
2019-05-13 20:45:43 -0400

    Update express router to remove redundant routes

--- router/index.js
+++ router/index.js
@@ -7,15 +7,10 @@ const pkg = require('../package.json')
 const filesrc = dir => path.join(process.cwd(), dir)
 const { TRANSIT_AGENCY = 'Miami-Dade Transit' } = process.env
 
-router.get('/realtime/TripUpdates.pb', (req, res) => res.sendFile(filesrc('/tmp/tripupdates.pb')))
-router.get('/realtime/VehiclePositions.pb', (req, res) => res.sendFile(filesrc('/tmp/vehicles.pb')))
-router.get('/gtfs.zip', (req, res) => res.sendFile(filesrc('/tmp/gtfs.zip')))
-
 router.all('/', (req, res) => {
   const template = fs.readFileSync(filesrc('./static/.index.html'), 'utf8')
   const intpl = template
     .replace(/(\$TRANSIT_AGENCY)/gm, TRANSIT_AGENCY)
-    .replace(/(\$PKGNAME)/gm, pkg.name)
     .replace(/(\$PKGVERSION)/gm, pkg.version)
   res.setHeader('Content-Type', 'text/html')
   res.status(200).send(intpl)
