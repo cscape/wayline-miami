e2412501a6c3e1dc1861c4785cacfc82a563244e
ciolt <cioltdev@outlook.com>
2019-07-21 18:56:58 -0400

    Fix issue with ETA Transit's servers being down... adds a fail-safe to return no vehicles if there's an API timeout of over 10 seconds. This way, the VP feed will still be generated but not include CG Trolley and not cause the app to throw crashes & errors

--- net/fetch-cgable.js
+++ net/fetch-cgable.js
@@ -3,15 +3,29 @@ const cfg = require('@wayline/config')
 const { Vehicles } = require('@wayline/transformer').ETATransit
 const basefeed = cfg.basefeeds.CoralGablesETA
 
-const getAllVehicles = async () => new Promise(
-  (resolve, reject) => axios.get(basefeed, {
+const CancelToken = axios.CancelToken
+const source = CancelToken.source()
+
+const getAllVehicles = async (timeout) => new Promise((resolve, reject) => {
+  setTimeout(() => { source.cancel('Coral Gables Trolley API timed out.') }, timeout)
+
+  axios.get(basefeed, {
+    cancelToken: source.token,
     params: { service: 'get_vehicles', token: 'TESTING' }
-  }).then(response => resolve(Vehicles(response.data)))
-    .catch(err => reject(err))
-)
+  }).then(response => {
+    try {
+      const fixup = Vehicles(response.data)
+      resolve(fixup)
+    } catch (_) {
+      // Fallback to report no vehicles
+      resolve([])
+    }
+  }).catch(() => resolve([]))
+})
 
 const Fetcher = async () => {
-  const vehicles = await getAllVehicles()
+  const timeout = 10000
+  const vehicles = await getAllVehicles(timeout)
   return vehicles
 }
 
