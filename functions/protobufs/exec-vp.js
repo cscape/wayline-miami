aedbc6c7cfe16545f73d11a3969b428aebf46a56
ciolt <cioltdev@outlook.com>
2019-07-27 22:57:46 -0400

    Update exec-vp to have error handling + make more generic about agency

--- functions/protobufs/exec-vp.js
+++ functions/protobufs/exec-vp.js
@@ -1,13 +1,27 @@
 const fs = require('fs')
 const path = require('path')
 const genVP = require('./gen-vp')
+const agencyModel = require('../../lib/agency-models')
 
-const SaveVehiclePositions = async () => {
-  console.log('Generating VehiclePositions')
-  const binaryData = await genVP()
-  const output = path.join(__dirname, `../../tmp/realtime/VehiclePositions.pb`)
-  await fs.writeFileSync(output, binaryData)
-  console.log('Saved VehiclePositions')
+/**
+ * Generates & saves VehiclePositions to tmp folder. Returns file location
+ * @param {string} agencyId The ID of a transit agency as in agency-models.js
+ */
+const SaveVehiclePositions = async (agencyId) => {
+  const { entityGenerateAsync, name } = agencyModel[agencyId]
+
+  console.log(`${name}: Generating VehiclePositions`)
+
+  const binaryData = await genVP(entityGenerateAsync, Date.now())
+  const output = path.join(__dirname, `../../tmp/realtime/${agencyId}.pb`)
+
+  try {
+    await fs.writeFileSync(output, binaryData)
+    console.log(`${name}: Saved VehiclePositions`)
+  } catch (err) {
+    console.error(`${name}: Failed to save VehiclePositions`, err)
+  }
+
   process.env.VP_UPDATE_COUNT = Number(process.env.VP_UPDATE_COUNT) + 1
   return output
 }
