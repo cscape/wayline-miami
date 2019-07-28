40b4374d2cde97277d0785de943bcf7a8582cf20
ciolt <cioltdev@outlook.com>
2019-07-27 23:10:44 -0400

    add agency support to refreshVP

--- functions/refresh-vp.js
+++ functions/refresh-vp.js
@@ -1,8 +1,10 @@
 const SaveVehiclePositions = require('./protobufs/exec-vp')
 
 // Recursively refresh VehiclePositions
-const RefreshVP = () => SaveVehiclePositions().then(() => {
-  setTimeout(RefreshVP, 10000)
+const RefreshVP = (agencyId) => SaveVehiclePositions().then(() => {
+  setTimeout(() => {
+    RefreshVP(agencyId)
+  }, 10000)
 })
 
 module.exports = RefreshVP
