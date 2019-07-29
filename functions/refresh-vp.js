4052be9003780446c2e477086b439ded4f434fc8
ciolt <cioltdev@outlook.com>
2019-07-28 22:44:03 -0400

    add agencyId to refresh-vp

--- functions/refresh-vp.js
+++ functions/refresh-vp.js
@@ -1,7 +1,7 @@
 const SaveVehiclePositions = require('./protobufs/exec-vp')
 
 // Recursively refresh VehiclePositions
-const RefreshVP = (agencyId) => SaveVehiclePositions().then(() => {
+const RefreshVP = (agencyId) => SaveVehiclePositions(agencyId).then(() => {
   setTimeout(() => {
     RefreshVP(agencyId)
   }, 10000)
